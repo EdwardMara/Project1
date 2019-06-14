cd41155b2036bbca1a8a4184b0d878933dc037f0
dnl092 <drogalsky14@my.whitworth.edu>
2019-06-14 13:48:43 -0700

    map fixed problem was duuuuuuumb

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -3,7 +3,10 @@
 // prompted by your browser. If you see the error "The Geolocation service
 // failed.", it means you probably did not give permission for the browser to
 // locate you.
+
+
 var map, infoWindow;
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('mapContainer'), {
     center: { lat: -34.397, lng: 150.644 },
