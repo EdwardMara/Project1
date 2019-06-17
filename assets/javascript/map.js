f0d229985c6750b825915cb62471bbfc22644f10
sofi.spag <sofi.spag@gmail.com>
2019-06-17 13:40:29 -0700

    update marks

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -28,12 +28,12 @@ function initMap() {
       var request = {
         location: pos,
         radius: '1000', // meters.
-        types: ['restaurants']
+        types: ['restaurant']
       };
     
       var service = new google.maps.places.PlacesService(map);
       service.nearbySearch(request, function(placeArray) {
-
+        
         for (let index = 0; index < placeArray.length && index < 5; index++) {
           const place = placeArray[index];
 
@@ -46,32 +46,36 @@ function initMap() {
             label: place.name,
             icon: image
           });
-  
+
           marker.setMap(map);
-            
+
+          var infoWindowFlag = new google.maps.InfoWindow ({
+            content: "This flag is near you!"
+          })
+  
+          google.maps.event.addListener(marker, 'click', function() {
+            infoWindowFlag.open(map,marker);
+            console.log('hello');
+          });
+
         }
 
+        // var infoWindowFlag = new google.maps.InfoWindow ({
+        //   content: "This flag is near you!"
+        // })
+
+        // google.maps.event.addListener(marker, 'click', function() {
+        //   infoWindowFlag.open(map,marker);
+        //   console.log('ive been clicked')
+        // });
+          
       });
-
-
-      // Adding image instead of marker
-      // var image = 'http://freepngimg.com/thumb/treasure/14-2-treasure-png-picture.png';
-      // var beachMarker = new google.maps.Marker({
-      //   position: pos,
-      //   map: map,
-      //   icon: image
-      // });
-      // var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
-      // var beachMarker = new google.maps.Marker({
-      //   position: pos,
-      //   map: map,
-      //   icon: image
-      // });
 
       infoWindow.setPosition(pos);
       infoWindow.setContent('You are here!');
       infoWindow.open(map);
       map.setCenter(pos);
+
     }, function () {
       handleLocationError(true, infoWindow, map.getCenter());
     });
