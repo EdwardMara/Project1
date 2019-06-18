fb99505b3dfae82c841723edfab5b290a74570ce
dnl092 <drogalsky14@my.whitworth.edu>
2019-06-18 10:29:13 -0700

    fixed undeclared database variable

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -11,6 +11,8 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+var database = firebase.database();
+
 // Note: This example requires that you consent to location sharing when
 // prompted by your browser. If you see the error "The Geolocation service
 // failed.", it means you probably did not give permission for the browser to
