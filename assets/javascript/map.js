a470ec12f3afa18078000e693c33197ea41ce337
Edward Mara <e.james.mara@gmail.com>
2019-06-19 12:35:39 -0700

    var declaration for string content

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -76,9 +76,10 @@ function initMap() {
       position: null,
       map: map,
       icon: iconBase + 'hiker_maps.png'
-
+      
     });
     
+    var contentStringProfile = '<div id="profileCard" class="card" style="width: 10rem;">' + '<div class="card-body text-center">' + '<img src="assets/images/pacman.png" width="45" height="30" class="d-inline-block align-top" alt="treasure">' + '<br>' + '<br>' + '<h6 class="card-subtitle mb-2 text-muted"> Your name</h6>' + '<p class="card-text">Points:' + "<span id='cardPoints'>" + currentXP + "</span>" + '</p>' + '<a href="profile.html" class="card-link">Go to my profile</a>' + '</div>' + '</div>';
     var infoWindow = new google.maps.InfoWindow({
       content: contentStringProfile
     });
@@ -107,7 +108,6 @@ function initMap() {
       });
 
 
-      var contentStringProfile = '<div id="profileCard" class="card" style="width: 10rem;">' + '<div class="card-body text-center">' + '<img src="assets/images/pacman.png" width="45" height="30" class="d-inline-block align-top" alt="treasure">' + '<br>' + '<br>' + '<h6 class="card-subtitle mb-2 text-muted"> Your name</h6>' + '<p class="card-text">Points:' + "<span id='cardPoints'>" + currentXP + "</span>" + '</p>' + '<a href="profile.html" class="card-link">Go to my profile</a>' + '</div>' + '</div>';
 
 
 
